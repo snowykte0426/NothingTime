a63262bf2e9b73dd9b30ffccd8f9d41e1dd96de4
snowykte0426 <snowykte0426@naver.com>
2024-09-16 16:51:46 +0900

    🗃feat: DB연결 클래스 추가
    DB와 연결을 수행하는 클래스를 추가했습니다

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+from config.config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    return app
+
+
+app = create_app()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ config/config.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    DB_USER = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
