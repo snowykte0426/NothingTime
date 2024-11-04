13c819ccb856d11e73f25ee0e7799d32411cdf95
snowykte0426 <snowykte0426@naver.com>
2024-11-04 23:48:11 +0900

    로그인 로직 재테스트

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template_string, redirect, url_for, g
 from flask_sqlalchemy import SQLAlchemy
 from config.config import Config
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -10,15 +10,28 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     return app
 
 app = create_app()
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, user_id, password):
+        self.user_id = user_id
+        self.password = generate_password_hash(password)  # 비밀번호 해시 저장
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+    db_instance = getattr(g, '_database', None)
+    if db_instance is not None:
+        db_instance.close()
 
 from page.main_page import main_page_code
 @app.route('/')
@@ -26,35 +39,38 @@ def hello_world():
     return render_template_string(main_page_code)
 
 from page.login_page import login_page_code
-@app.route("/login",methods=['GET'])
+@app.route("/login", methods=['GET'])
 def login_page():
     return render_template_string(login_page_code)
 
 from page.signup_page import signup_page_code
-@app.route("/signup",methods=['GET'])
+@app.route("/signup", methods=['GET'])
 def signup_page():
     return render_template_string(signup_page_code)
-
-from model import User
 
 @app.route("/login", methods=['POST'])
 def login():
     user_id = request.form.get('ID')
     password = request.form.get('Password')
-
-    # 사용자 정보 조회
     user = User.query.filter_by(user_id=user_id).first()
-
     if user and check_password_hash(user.password, password):
-        return redirect(url_for('welcome'))  # 로그인 성공 시 환영 페이지로 리디렉션
+        return redirect(url_for('welcome'))
     else:
-        return "로그인 실패. 다시 시도해주세요.", 401  # 로그인 실패 시 메시지 반환
+        return "로그인 실패. 다시 시도해주세요.", 401
 
 @app.route("/welcome")
 def welcome():
     return "로그인 성공! 환영합니다."
 
+@app.route("/signup", methods=['POST'])
+def signup():
+    user_id = request.form.get('ID')
+    password = request.form.get('Password')
+    new_user = User(user_id=user_id, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('login_page'))
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
--- model/User.py
+++ model/User.py
@@ -8,3 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, user_id, password):
+        self.user_id = user_id
+        self.password = password
